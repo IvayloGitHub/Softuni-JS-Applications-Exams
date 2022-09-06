b84a494a36e6cda34bd35495893d28fb237077e5
Ivaylo <69982904+IvayloGitHub@users.noreply.github.com>
2022-09-06 16:37:43 +0300

    Add files via upload

---
+++ JS Applications Exam Preparation - Meme Lounge/src/notification.js
@@ -0,0 +1,10 @@
+const box = document.querySelector('#errorBox');
+
+export function notify(message) {
+    box.innerHTML = `<span>${message}</span>`;
+    box.style.display = 'block';
+
+    setTimeout(() => {
+        box.style.display = 'none';
+    }, 3000);
+}
